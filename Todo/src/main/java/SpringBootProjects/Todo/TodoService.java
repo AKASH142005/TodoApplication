467203a6a2252be1bb0b89a5b5b250cd98fbafe9
Akash <akashanbarasuz@gmail.com>
2026-06-29 17:19:42 +0530

    create todo

---
+++ Todo/src/main/java/SpringBootProjects/Todo/TodoService.java
@@ -0,0 +1,18 @@
+package SpringBootProjects.Todo;
+
+import SpringBootProjects.Todo.Models.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+    public Todo createTodo(Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    public Todo getTodoById(Long id){
+        return todoRepository.findById(id).orElseThrow(()-> new RuntimeException("Not found"));
+    }
+}
