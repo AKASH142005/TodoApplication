2433de3ba38b68b9894c7499ef6bf73cb44e6efe
Akash <akashanbarasuz@gmail.com>
2026-07-01 20:26:47 +0530

    add the valid annotation for datasource for data validation

--- Todo/src/main/java/SpringBootProjects/Todo/TodoController.java
+++ Todo/src/main/java/SpringBootProjects/Todo/TodoController.java
@@ -1,29 +1,42 @@
 package SpringBootProjects.Todo;
 
 import SpringBootProjects.Todo.Models.Todo;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/todo/api")
 public class TodoController {
     @Autowired
     private TodoService todoService;
     @PostMapping("/create")
-    public ResponseEntity<Todo> createUser(@RequestBody Todo todo){
+    public ResponseEntity<Todo> createTodo(@Valid @RequestBody Todo todo){
         return new ResponseEntity<>(todoService.createTodo(todo), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Todo> getUser(@PathVariable Long id){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201" , description = "Todo Retrieved successfully"),
+            @ApiResponse(responseCode = "404" , description = "Todo was Not found")
+    })
+    public ResponseEntity<Todo> getGetTodoById(@PathVariable Long id){
         try {
             Todo getTodo = todoService.getTodoById(id) ;
             return new ResponseEntity<>(getTodo , HttpStatus.OK);
         } catch (RuntimeException error){
+            log.info("Error");
+            log.warn("");
+            log.error("error found" , error);
             return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
         }
     }
@@ -31,6 +44,11 @@ public class TodoController {
     @GetMapping
     public ResponseEntity<List<Todo>> getAllTodos(){
         return new ResponseEntity<>(todoService.allTodos() ,HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<Todo>> getAllTodosPages(@RequestParam int page , @RequestParam int size){
+        return new ResponseEntity<>(todoService.getAllTodosPages(page , size) , HttpStatus.OK);
     }
 
     @PutMapping
