f81690107581ad4113de4dfa7c85420127ae1404
Akash <akashanbarasuz@gmail.com>
2026-07-01 20:25:46 +0530

    add Validation

--- Todo/src/main/java/SpringBootProjects/Todo/Models/Todo.java
+++ Todo/src/main/java/SpringBootProjects/Todo/Models/Todo.java
@@ -1,18 +1,34 @@
 package SpringBootProjects.Todo.Models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+// @Getter @ Setter
 public class Todo {
     @Id
     @GeneratedValue
     private Long id ;
+    @NotBlank
+    @Schema(name = "title" , example = "Complete Spring Boot")
     String title ;
+    @NotBlank
     String description ;
     boolean isCompleted ;
+
+    // @Pattern(regexp = "^[0-9]{10}$")
+    // String PhoneNumber ;
+    // @Email
+    // String email ;
 }
