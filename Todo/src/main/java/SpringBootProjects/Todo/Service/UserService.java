f1234b307cc0a6b082a94cecafbe814c0c14976c
Akash <akashanbarasuz@gmail.com>
2026-07-02 21:38:49 +0530

    add find by email

---
+++ Todo/src/main/java/SpringBootProjects/Todo/Service/UserService.java
@@ -0,0 +1,22 @@
+package SpringBootProjects.Todo.Service;
+
+
+
+import SpringBootProjects.Todo.Models.User;
+import SpringBootProjects.Todo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository ;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Not Found"));
+    }
+}
