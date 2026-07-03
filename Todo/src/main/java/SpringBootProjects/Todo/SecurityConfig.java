42a8362a0d39c1d3c26941d6619f8a951518690b
Akash <akashanbarasuz@gmail.com>
2026-07-03 19:36:21 +0530

    add one more layer for authenticated

--- Todo/src/main/java/SpringBootProjects/Todo/SecurityConfig.java
+++ Todo/src/main/java/SpringBootProjects/Todo/SecurityConfig.java
@@ -2,12 +2,17 @@ package SpringBootProjects.Todo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -15,13 +20,29 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http , JwtFilter jwtFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer ::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
+                )
+                .addFilterBefore(jwtFilter , UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+       CorsConfiguration configuration = new CorsConfiguration();
+       configuration.setAllowedOrigins(List.of("*"));
+       configuration.setAllowedHeaders(List.of("*"));
+       configuration.setAllowedMethods(List.of("POST" , "DELETE", "PUT" , "GET"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**" , configuration);
+
+        return source ;
     }
 }
