f48af0e10d30f6daac5b00cc5f8cb1a4e430c2b1
Akash <akashanbarasuz@gmail.com>
2026-07-06 19:55:52 +0530

    modify

--- Todo/src/main/java/SpringBootProjects/Todo/Controller/TodoController.java
+++ Todo/src/main/java/SpringBootProjects/Todo/Controller/TodoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/api/todo")
 public class TodoController {
     @Autowired
     private TodoService todoService;
